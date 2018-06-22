31ed096b0046c9cac7456e39cf7f0c208334fa8e
PravallikaChodisetty <40449889+PravallikaChodisetty@users.noreply.github.com>
2018-06-22 09:25:22 +0000

    Add files via upload

---
+++ pro1/js/data.js
@@ -0,0 +1,77 @@
+/* Model Data */
+
+var locations = [
+    
+    {
+        title: 'Trinetra Ganesh  Temple',
+        location: {
+            lat: 26.0175, 
+            lng: 76.4532
+        }
+    },
+
+    {
+        title: 'Mahakaleshwar Temple',
+        location: {
+            lat: 23.1827, 
+            lng: 75.7682
+        }
+    },
+    {
+        title: 'Omkareshwar Temple',
+        location: {
+            lat: 22.2456,
+            lng: 76.1510
+        }
+    },
+    {
+        title: 'Kedarnath Temple',
+        location: {
+            lat: 30.7352,
+            lng: 79.0669
+        }
+    },
+   
+    {
+        title: 'Bhimshankar Temple',
+        location: {
+            lat: 19.072,
+            lng: 73.5357 
+        }
+    },
+     {
+        title: 'Trimbakeshwar Temple',
+        location: {
+            lat: 19.9322,
+            lng: 73.5307
+        }
+    },
+     {
+        title: 'Baidyanath Temple',
+        location: {
+            lat: 24.4925,
+            lng: 86.7002 
+        }
+    },
+     {
+        title: 'Nageshvara Temple',
+        location: {
+            lat: 22.3360,
+            lng: 69.0870
+        }
+    },
+    {
+        title: 'Ramanatha Swamy  Temple',
+        location: {
+            lat: 9.2881,
+            lng: 79.3174
+        }
+    },
+    {
+        title: 'Grishneshwar Temple',
+        location: {
+            lat: 20.0249,
+            lng: 75.1698
+        }
+    }
+];
